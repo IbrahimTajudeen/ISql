10b3df4efb04f82ec499633d068d743f18acc6e1
Ibrahim Tajudeen <101503731+IbrahimTajudeen@users.noreply.github.com>
2024-11-25 13:22:38 +0100

    Add files via upload

---
+++ isac.isql.collections/interface/ITreeMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isac.Isql.Collections
+{
+    interface ITreeMap 
+    {
+        void AddChildNode(Cell parent, Cell child);
+        void RemoveChild(Cell parent);
+        Cell GetChild(int child_index);
+        /// <summary>
+        /// this should goto TreeMap
+        /// </summary>
+        /// <param name="chid_node"></param>
+        /// <returns></returns>
+        Cell GetParent(Cell chid_node);
+        Cell GetFirstChild();
+        Cell GetLastChild();
+
+    }
+}
