7733ba4735faf6cd6a69d9134469ea74978c7186
Ibrahim Tajudeen <101503731+IbrahimTajudeen@users.noreply.github.com>
2024-11-25 13:17:45 +0100

    Update celladd.cs

--- isac.isql.collections/event/celladd.cs
+++ isac.isql.collections/event/celladd.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
+namespace Isac.Isql.Collections
+{
+	//RowLevel
+    public sealed class CellAddEventArgs : EventArgs 
+    {         
+        public Cell InputCell;
+        //public Row AffectedRow;
+        
+    }
+}
