2d5d5d9a662ed6a07160876d1564db9692d24a06
Ibrahim Tajudeen <101503731+IbrahimTajudeen@users.noreply.github.com>
2024-11-25 13:20:21 +0100

    Update ITable.cs

--- isac.isql.collections/interface/ITable.cs
+++ isac.isql.collections/interface/ITable.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Isac.Isql.Collections
+{
+    interface ITable
+    {
+        void DataTypeConverter(string[] datatype_to_chechk, string[] orginal_types);
+        
+    }
+}
