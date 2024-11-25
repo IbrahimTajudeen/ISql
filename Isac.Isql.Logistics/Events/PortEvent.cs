8dce4620d534bf9b433b9c7041c5b61f7650643b
Ibrahim Tajudeen <101503731+IbrahimTajudeen@users.noreply.github.com>
2024-11-25 13:29:59 +0100

    Add files via upload

---
+++ Isac.Isql.Logistics/Events/PortEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Isac.Isql.Logistics
+{
+    internal class PortEvent : EventArgs
+    {         
+        public List<dynamic> Data = new List<dynamic>();
+        public List<string> Tree = new List<string>();
+        public Dictionary<int, dynamic[]> Ports = new Dictionary<int, dynamic[]>();
+        public Collections.DataTable DTable = null;
+    }
+}
