b4974fcde03dfd907fb166d5a31ebf004f67ecb6
Ibrahim Tajudeen <101503731+IbrahimTajudeen@users.noreply.github.com>
2024-11-25 13:28:34 +0100

    Update ILogic.cs

--- Isac.Isql.Logistics/Events/ILogic.cs
+++ Isac.Isql.Logistics/Events/ILogic.cs
@@ -1,1 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Isac.Isql.Logistics
+{
+    interface ILogic
+    {
+    }
+}
